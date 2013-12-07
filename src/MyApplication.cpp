eac31815f37a8debbee353c98347206b28d4f4ce
Vladimír Vondruš <mosra@centrum.cz>
2013-12-07 22:09:13 +0100

    Adapted to Platform changes.
    Shorter code!

--- src/MyApplication.cpp
+++ src/MyApplication.cpp
@@ -7,17 +7,12 @@ class MyApplication: public Platform::Application {
     public:
         explicit MyApplication(const Arguments& arguments);
 
-    protected:
-        void viewportEvent(const Vector2i& size) override;
+    private:
         void drawEvent() override;
 };
 
 MyApplication::MyApplication(const Arguments& arguments): Platform::Application(arguments) {
     /* TODO: Add your initialization code here */
-}
-
-void MyApplication::viewportEvent(const Vector2i& size) {
-    defaultFramebuffer.setViewport({{}, size});
 }
 
 void MyApplication::drawEvent() {
