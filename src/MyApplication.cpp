85f4f837e7cf922e26d2b1ce11cb6b8776a910cc
Vladimír Vondruš <mosra@centrum.cz>
2014-01-11 01:20:36 +0100

    Using absolute includes.

--- src/MyApplication.cpp
+++ src/MyApplication.cpp
@@ -1,5 +1,5 @@
-#include <Platform/Sdl2Application.h>
-#include <DefaultFramebuffer.h>
+#include <Magnum/DefaultFramebuffer.h>
+#include <Magnum/Platform/Sdl2Application.h>
 
 using namespace Magnum;
 
