8d999c6c42b93b6dabd4705ca17062170350e991
Vladimír Vondruš <mosra@centrum.cz>
2014-03-07 00:53:46 +0100

    Alphabetically reorder includes.

--- src/MyApplication.cpp
+++ src/MyApplication.cpp
@@ -1,5 +1,5 @@
-#include <Magnum/Platform/GlutApplication.h>
 #include <Magnum/DefaultFramebuffer.h>
+#include <Magnum/Platform/GlutApplication.h>
 #include <Magnum/SceneGraph/Scene.h>
 #include <Magnum/SceneGraph/MatrixTransformation3D.h>
 #include <Magnum/SceneGraph/Camera3D.h>
