93ec12fa096b82617ef876514c0db226596116a7
Vladimír Vondruš <mosra@centrum.cz>
2015-06-23 21:44:56 +0200

    Using {} instead of () for object construction.

--- src/MyApplication.cpp
+++ src/MyApplication.cpp
@@ -15,7 +15,7 @@ class MyApplication: public Platform::Application {
         void drawEvent() override;
 };
 
-MyApplication::MyApplication(const Arguments& arguments): Platform::Application(arguments) {
+MyApplication::MyApplication(const Arguments& arguments): Platform::Application{arguments} {
     /* TODO: Add your initialization code here */
 }
 
