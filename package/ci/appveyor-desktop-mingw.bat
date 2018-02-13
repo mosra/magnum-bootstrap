ad846e30baf800bbc66cdcaf91ec6439aa14c48a
Vladimír Vondruš <mosra@centrum.cz>
2018-02-13 18:48:07 +0100

    package/ci: use preinstalled MinGW on AppVeyor.

--- package/ci/appveyor-desktop-mingw.bat
+++ package/ci/appveyor-desktop-mingw.bat
@@ -1,8 +1,8 @@
 rem Workaround for CMake not wanting sh.exe on PATH for MinGW. AARGH.
 set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
-set PATH=C:\tools\mingw64\bin;%APPVEYOR_BUILD_FOLDER%\deps\bin;%PATH%
+set PATH=C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1\mingw64\bin;%APPVEYOR_BUILD_FOLDER%\deps\bin;%PATH%
 
-rem Build Corrade. Could not get Ninja to work, meh.
+rem Build Corrade
 git clone --depth 1 git://github.com/mosra/corrade.git || exit /b
 cd corrade || exit /b
 mkdir build && cd build || exit /b
@@ -12,9 +12,9 @@ cmake .. ^
     -DWITH_INTERCONNECT=OFF ^
     -DWITH_TESTSUITE=OFF ^
     -DBUILD_DEPRECATED=OFF ^
-    -G "MinGW Makefiles" || exit /b
-cmake --build . -- -j || exit /b
-cmake --build . --target install -- -j || exit /b
+    -G Ninja || exit /b
+cmake --build . || exit /b
+cmake --build . --target install || exit /b
 cd .. && cd ..
 
 rem Build Magnum
@@ -35,9 +35,9 @@ cmake .. ^
     -DWITH_TEXT=OFF ^
     -DWITH_TEXTURETOOLS=OFF %EXTRA% ^
     -DBUILD_DEPRECATED=OFF ^
-    -G "MinGW Makefiles" || exit /b
-cmake --build . -- -j || exit /b
-cmake --build . --target install -- -j || exit /b
+    -G Ninja || exit /b
+cmake --build . || exit /b
+cmake --build . --target install || exit /b
 cd .. && cd ..
 
 rem Build bootstrap project
@@ -48,5 +48,5 @@ cmake .. ^
     -DCMAKE_PREFIX_PATH="%APPVEYOR_BUILD_FOLDER%/SDL" ^
     -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -G "MinGW Makefiles" || exit /b
-cmake --build . -- -j || exit /b
+    -G Ninja || exit /b
+cmake --build . || exit /b
