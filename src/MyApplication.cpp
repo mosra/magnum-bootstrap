c60f6d8efe937a5fc96764a5a13c7046382c0a6f
jackcamp <jackcamp@vt.edu>
2020-01-20 22:34:05 -0500

    Began adding functions to the MagnumWidget class.

--- src/MyApplication.cpp
+++ src/MyApplication.cpp
@@ -2,14 +2,36 @@
 
 #include <gtkmm/application.h>
 #include <gtkmm/glarea.h>
+#include <gtkmm/window.h>
 
 
 class MagnumWidget : public Gtk::GLArea {
 
 public:
 
+    explicit MagnumWidget() {
 
-private:
+        set_size_request(1024, 1024);
+
+        signal_realize().connect(sigc::mem_fun(this, &MagnumWidget::onRealize));
+        signal_render().connect(sigc::mem_fun(this, &MagnumWidget::onRender));
+        signal_unrealize().connect(sigc::mem_fun(this, &MagnumWidget::onUnrealize));
+    }
+
+
+protected:
+
+    void onRealize() {
+
+    }
+
+    bool onRender(const Glib::RefPtr<Gdk::GLContext>& context) {
+
+    }
+
+    void onUnrealize() {
+
+    }
 
 
 };
@@ -19,5 +41,9 @@ int main(int argc, char **argv) {
 
     auto app = Gtk::Application::create(argc, argv, "graphics.magnum.bootstrap.base-gtkmm");
 
-    return app->run();
+    auto widget = MagnumWidget();
+    auto window = Gtk::Window();
+    window.add(widget);
+
+    return app->run(window);
 }
