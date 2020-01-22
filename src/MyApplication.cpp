9d1cea55151eb00e4457ecb0ab3c4352eb5a72e2
jackcamp <jackcamp@vt.edu>
2020-01-22 01:15:52 -0500

    Added more comments, similar to those in base-qt

--- src/MyApplication.cpp
+++ src/MyApplication.cpp
@@ -12,11 +12,22 @@ class MagnumWidget : public Gtk::GLArea {
 
 public:
 
-    explicit MagnumWidget(Platform::GLContext& context) : Gtk::GLArea(), _context(context) {
+    explicit MagnumWidget(Platform::GLContext &context) : Gtk::GLArea(), _context(context) {
 
+        /* This widget will re-render everything every time it needs to be drawn */
         set_auto_render();
+
+        /* Setting size requests and scaling behavior */
+        set_hexpand();
+        set_vexpand();
+        set_halign(Gtk::ALIGN_FILL);
+        set_valign(Gtk::ALIGN_FILL);
         set_size_request(1024, 1024);
 
+        /* Setting desired OpenGL version */
+        set_required_version(4, 6);
+
+        /* Connecting signals to their respective handlers */
         signal_realize().connect(sigc::mem_fun(this, &MagnumWidget::onRealize));
         signal_render().connect(sigc::mem_fun(this, &MagnumWidget::onRender));
         signal_unrealize().connect(sigc::mem_fun(this, &MagnumWidget::onUnrealize));
@@ -28,24 +39,23 @@ protected:
     void onRealize() {
         _context.create();
 
+        /* TODO: Add your initialization code here */
+
     }
 
-    bool onRender(const Glib::RefPtr<Gdk::GLContext>& context) {
+    bool onRender(const Glib::RefPtr<Gdk::GLContext> &context) {
 
-
+        /* Reset state to avoid Gtkmm affecting Magnum */
         GL::Context::current().resetState(GL::Context::State::ExitExternal);
-        {
 
-            // FIXME How do I get the default framebuffer ID?
-            auto gtkmmDefaultFramebuffer = GL::Framebuffer::wrap(0, {{}, {get_width(), get_height()}});
+        /* FIXME: How do I get the default framebuffer ID? */
+        auto gtkmmDefaultFramebuffer = GL::Framebuffer::wrap(0, {{}, {get_width(), get_height()}});
 
+        gtkmmDefaultFramebuffer.clear(GL::FramebufferClear::Color);
 
-            gtkmmDefaultFramebuffer.clearColor(0, Color4{0, 0.5, 1, 1});
+        /* TODO: Add your drawing code here */
 
-            gtkmmDefaultFramebuffer
-                    .clear(GL::FramebufferClear::Color|GL::FramebufferClear::Depth)
-                    .bind();
-        }
+        /* Clean up Magnum state and back to Gtkmm */
         GL::Context::current().resetState(GL::Context::State::EnterExternal);
 
         return true;
@@ -53,11 +63,12 @@ protected:
 
     void onUnrealize() {
 
+        /* TODO Clean up here */
     }
 
 private:
 
-    Platform::GLContext& _context;
+    Platform::GLContext &_context;
 
 };
 
